23467bd4853f324b6e37e02c05ba1602769d7b92
duby12345 <dubydov123@gmail.com>
2020-09-22 12:31:03 +0200

    Testing

---
+++ targil5.py
@@ -0,0 +1,16 @@
+#lists
+
+#new_list=[2,6.6,"dubu iancu",[]]
+#print(new_list)
+#new_list2=new_list+[77, 'hello world']
+#print(new_list2)
+#new_list3=new_list2+new_list
+#print(new_list3)
+#new_list4=new_list2*2
+#print(new_list4)
+#new_list5=[2,6.6,"duby iancu",44]
+#print(new_list5[0])
+#print(new_list5[2])
+my_list6=[1,2,3,6.6,"dudu cohen"]
+#print("my name: " + my_list6[4])
+print("my name: " + str(my_list6[2]))
