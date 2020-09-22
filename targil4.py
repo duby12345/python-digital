cb6afb948d3aec401af5e9d09683f121da03032f
duby12345 <dubydov123@gmail.com>
2020-09-22 12:33:00 +0200

    Testing

---
+++ targil4.py
@@ -0,0 +1,7 @@
+name="duby iancu"
+age= 28
+mail="duby@gmail.com"
+
+print("Full name: " + name +"\nAge: " + str(age) + "\nMail: " + mail)
+print("\n\nFull name: " + name[::-1] +"\nAge: " + str(age*3))
+print("duby" in "idan dror aviv rahel duby yoval moshe")
